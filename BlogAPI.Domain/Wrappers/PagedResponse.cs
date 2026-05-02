c07d75b32f7ca9991bb75112abf7b6bab2535e9d
Meet Rajpal <meetrajpal.linkedin@gmail.com>
2026-05-02 19:17:14 +0530

    added common api response and paginated response and also pagination filter for incoming requests

---
+++ BlogAPI.Domain/Wrappers/PagedResponse.cs
@@ -0,0 +1,24 @@
+﻿namespace BlogAPI.Domain.Wrappers;
+
+public class PagedResponse<T> : ApiResponse<T>
+{
+    public int PageNumber { get; set; }
+    public int PageSize { get; set; }
+    public int TotalRecords { get; set; }
+    public int TotalPages => (int)Math.Ceiling((double)TotalRecords / PageSize);
+    public bool HasNextPage => PageNumber < TotalPages;
+    public bool HasPreviousPage => PageNumber > 1;
+
+    public static PagedResponse<T> Success(T data, int pageNumber, int pageSize, int totalRecords, string message = "")
+    {
+        return new PagedResponse<T>
+        {
+            Succeeded = true,
+            Message = message,
+            Data = data,
+            PageNumber = pageNumber,
+            PageSize = pageSize,
+            TotalRecords = totalRecords
+        };
+    }
+}
